fe22c89ea5910e67cb9988edf762c27ff8beff9b
Joffrey Michaud <joffrey.michaud@u-bourgogne.fr>
2025-05-20 15:25:49 +0200

    fix git pull

--- xview/update/update_project.py
+++ xview/update/update_project.py
@@ -1,17 +1,18 @@
 import subprocess
 import functools
-
+import os
 
 _warned_once = False
 
 def is_up_to_date() -> bool:
     """Vérifie si le dépôt local est à jour avec la branche distante."""
+    REPO_DIR = os.path.dirname(os.path.abspath(__file__))
     try:
-        subprocess.run(["git", "fetch"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
+        subprocess.run(["git", "fetch"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, cwd=REPO_DIR)
 
-        local = subprocess.check_output(["git", "rev-parse", "@"], stderr=subprocess.PIPE).strip()
-        remote = subprocess.check_output(["git", "rev-parse", "@{u}"], stderr=subprocess.PIPE).strip()
-        base = subprocess.check_output(["git", "merge-base", "@", "@{u}"], stderr=subprocess.PIPE).strip()
+        local = subprocess.check_output(["git", "rev-parse", "@"], stderr=subprocess.PIPE, cwd=REPO_DIR).strip()
+        remote = subprocess.check_output(["git", "rev-parse", "@{u}"], stderr=subprocess.PIPE, cwd=REPO_DIR).strip()
+        base = subprocess.check_output(["git", "merge-base", "@", "@{u}"], stderr=subprocess.PIPE, cwd=REPO_DIR).strip()
 
         return local == remote
     except subprocess.CalledProcessError as e:
@@ -39,7 +40,8 @@ def warn_if_outdated(obj):
 def pull_latest_changes():
     """Effectue un git pull pour récupérer les dernières modifications."""
     try:
-        subprocess.run(["git", "pull"], check=True)
+        REPO_DIR = os.path.dirname(os.path.abspath(__file__))
+        subprocess.run(["git", "pull"], check=True, cwd=REPO_DIR)
         print("Projet mis à jour avec succès.")
     except subprocess.CalledProcessError:
         print("/!\\ Échec du git pull.")
